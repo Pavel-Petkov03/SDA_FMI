6ac7ad1383ad7160790c50bb902ff2d81a5bf5db
Pavel Petkov <elias5247698@abv.bg>
2024-09-06 21:59:41 +0300

    Add project files.

---
+++ SDA_FMI/main.cpp
@@ -0,0 +1,12 @@
+// SDA_FMI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World12!\n";
+    for(int i = 0; i < 5; i++){
+
+    }
+}
