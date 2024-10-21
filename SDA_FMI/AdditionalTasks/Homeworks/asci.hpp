d22a7082dcd033ea34564191ab666bcd9c0b4248
Pavel Petkov <elias5247698@abv.bg>
2024-10-21 16:07:28 +0300

    adding some exercises

--- SDA_FMI/AdditionalTasks/Homeworks/asci.hpp
+++ SDA_FMI/AdditionalTasks/Homeworks/asci.hpp
@@ -5,19 +5,6 @@
 #include <algorithm>
 using namespace std;
 
-char fromIndexToChar(int index) {
-    if (index >= 0 && index <= 9) {
-        return '0' + index;
-    }
-    else if (index >= 10 && index <= 35) {
-        return 'A' + (index - 10);
-    }
-    else if (index >= 36 && index <= 61) {
-        return 'a' + (index - 36);
-    }
-    return 'a';
-}
-
 int fromCharToIndex(char ch) {
     if (ch >= '0' && ch <= '9') {
         return ch - '0';
@@ -31,7 +18,7 @@ int fromCharToIndex(char ch) {
     return 0;
 }
 
-void  printIndexes() {
+void countOnlyOne() {
     string s;
     std::cin >> s;
     std::pair<int, int> countArr[26 + 26 + 10];
@@ -43,9 +30,10 @@ void  printIndexes() {
         countArr[currentIndex].second = i;
         countArr[currentIndex].first++;
     }
-    for (int i = 0; i < 62; i++) {
-        if (countArr[i].first == 1) {
-            std::cout << countArr[i].second;
+    for (int i = 0; i < s.size(); i++) {
+        int currentIndex = fromCharToIndex(s[i]);
+        if (countArr[currentIndex].first == 1) {
+            std::cout << countArr[currentIndex].second;
             if (i != s.size() - 1) {
                 std::cout << " ";
             }
